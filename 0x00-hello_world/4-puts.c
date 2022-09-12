c6dd011e7e98b4b6e47674ddb2c3d3fbb223c47b
ANATHIMHLANGA <mhlanga.anathi@outlook.com>
2022-09-12 07:47:37 -0700

    ...... 0x0-shell basics

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - prints using puts() command
+* return: 0 prints something
+*/
+int main(void)
+{
+	puts("\"programmingis like a multilingual puzzle");
+	return (0);
+}
