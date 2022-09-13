f10eba4dd40e292c6c7bbea39b6d5afe69ecc98f
ANATHIMHLANGA <mhlanga.anathi@outlook.com>
2022-09-13 08:45:36 -0700

    ...... 0x0-shell basics

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,11 +4,12 @@
 /**
  * main - Entry point
  *
- * Return: Always (success)
+ * Return: Always success
  */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n + rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -17,5 +18,6 @@ int main(void)
 		printf("%d is zero\n", n);
 	else  
 		printf("%d is negative\n", n);
+
 	return (0)
 }
