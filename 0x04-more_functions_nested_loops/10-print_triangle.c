fdc73b270750492b9438792fe72e91432e096518
ANATHIMHLANGA <mhlanga.anathi@outlook.com>
2022-09-22 14:44:30 -0700

    ...... 0x0-shell basics

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,31 +1,29 @@
 #include "main.h"
-
 /**
  * print_triangle - function print
- * @size: size of triangl=e
+ * @size: size of triangle
  *
- * Return: nothinh fail
+ * Return: nothing fail
  */
 
 void print_triangle(int size)
 {
 	int inc1, inc2;
-
+	
 	if (size > 0)
 	{
 		for (inc1 = 1; inc1 <= size; inc1++)
-			{
-				for ((inc2 = size - inc1); inc2 > 0; inc2--)
-					_putchar(' ');
-
-				for (inc2 = 0; inc2 < inc1; inc2++)
-					_putchar('#');
-
-				if (inc1 == size)
-					continue;
-
-				_putchar('\n');
-			}
-	}
+		{
+			for ((inc2 = size - inc1); inc2 > 0; inc2--)
+				_putchar(' ');
+			
+			for (inc2 = 0; inc2 < inc1; inc2++)
+				_putchar('#');
+			
+			if (inc1 == size)
+				continue;
 			_putchar('\n');
+		}
+	}
+	_putchar('\n');
 }
