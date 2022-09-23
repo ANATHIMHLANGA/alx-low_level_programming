d22106a0d5f57c4addbfe76bcf21ae6e14b00fc4
ANATHIMHLANGA <mhlanga.anathi@outlook.com>
2022-09-22 20:37:37 -0700

    ...... 0x0-shell basics

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * main - header file for protoypes
+ * void protoypes (void)
+ * int protoypes (int)
+ */
+
+int _putchar(char c);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncat(char *dest, char *src, int n);
+void reverse_array(int *a, int n);
+char *_strcat(char *dest, char *src);
+char *string_toupper(char *);
+char *cap_string(char *);
+
+#endif /* MAIN_H */
