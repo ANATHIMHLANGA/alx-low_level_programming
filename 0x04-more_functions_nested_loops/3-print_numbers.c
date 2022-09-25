e30347acc5f57e085d97cd2ff0008c8c35bec1c8
ANATHIMHLANGA <mhlanga.anathi@outlook.com>
2022-09-25 15:04:02 -0700

    ...... 0x0-shell basics

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * print_numbers - function that print all numbers from
- * 0 to 9
+ * print_numbers - function that print all numbers from 0 to 9
  *
  * Return: returns nothing
  */
@@ -9,9 +8,7 @@
 void print_numbers(void)
 {
 	int number = 48;
-
 	for (number = 48; number < 58; number++)
 		_putchar(number);
-	
 	_putchar(10);
 }
