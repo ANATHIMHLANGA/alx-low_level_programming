f8ce07c95bd67e2ce0c1b8766f5285c21157d837
ANATHIMHLANGA <mhlanga.anathi@outlook.com>
2022-09-30 17:34:32 -0700

    0x08-recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _sqrt_recursion - returns the natural square root of a number
+* main - returns the natural square root of a number
 * @n: number to be used
 *
 * Return: the square root of n
