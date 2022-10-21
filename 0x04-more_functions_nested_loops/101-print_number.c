0f5c5dc6efda7a4ea4123e30e40da729c63ba674
ANATHIMHLANGA <mhlanga.anathi@outlook.com>
2022-10-21 00:05:17 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,17 +4,17 @@
 * @n: The number to print
 *
 */
-	void print_number(int n)
+void print_number(int n)
 {
-	unsigned int num = n;
-	if (n < 0)
+unsigned int num = n;
+if (n < 0)
 {
-	_putchar('-');
-	num = -num;
+_putchar('-');
+num = -num;
 }
-	if (num > 9)
+if (num > 9)
 {
-	print_number(num / 10);
+print_number(num / 10);
 }
-	_putchar(num % 10 + '0');
+_putchar(num % 10 + '0');
 }
